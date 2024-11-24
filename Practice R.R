2ae1be88897d545e6d1929989ec2be2f4545d217
mrndolo <callistomutunga@gmail.com>
2024-11-24 17:42:54 -0500

    R practice continued

---
+++ Practice R.R
@@ -0,0 +1,23 @@
+y <-20
+z <-y+5
+print(z)
+
+a<-10
+result <- (a*4)/2
+print(result)
+
+#creating a vector
+my_vector <- c(1,2,3,4,5)
+print(my_vector + 2)
+
+#another vector
+vector2<- c(5,10,15)
+newvector <-sum(vector2 + 3)
+newvector
+
+#sum of squared differences from the mean
+x<-c(1,-1,3.5,2)
+x
+varofx<-sum((x-mean(x))^2)
+varofx
+
